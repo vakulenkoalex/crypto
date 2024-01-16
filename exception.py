44d7bb2b369ed3992f7e09691cb38fc4aba0aea6
Вакуленко Алексей <ayv@pharmsklad.ru>
2024-01-16 15:58:18 +0400

    добавил конструктор и представление ошибки

--- exception.py
+++ exception.py
@@ -1,3 +1,8 @@
 
 class CryptoException(Exception):
-    pass
+    def __init__(self, class_name, message):
+        self._class_name = class_name
+        self._message = message
+
+    def __format__(self, format_spec):
+        return f'{self._message} ({self._class_name}))'
