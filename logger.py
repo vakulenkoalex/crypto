ce0183e2219926a0eaf0e36b67b35a796bbc6971
Вакуленко Алексей <ayv@pharmsklad.ru>
2024-01-16 17:30:07 +0400

    костыль, чтобы не дублировались запись в файлы

--- logger.py
+++ logger.py
@@ -5,10 +5,11 @@ class CryptoLogger:
     def __init__(self, name, log_file):
         self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s (%(process)5d) - %(message)s')
-        file_handler = logging.FileHandler(log_file, encoding='utf8')
-        file_handler.setFormatter(formatter)
-        self.logger.addHandler(file_handler)
+        if len(self.logger.handlers) == 0:
+            file_handler = logging.FileHandler(log_file, encoding='utf8')
+            formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s (%(process)5d) - %(message)s')
+            file_handler.setFormatter(formatter)
+            self.logger.addHandler(file_handler)
 
     def info(self, message):
         self.logger.info(message)
