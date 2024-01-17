c0c71ecad14b8e04bb6ca32eca445b330b190f5e
Вакуленко Алексей <ayv@pharmsklad.ru>
2024-01-17 12:03:47 +0400

    добавил пропуск сообщений по времени

--- get_telegram_message.py
+++ get_telegram_message.py
@@ -1,3 +1,6 @@
+import datetime
+
+import pytz
 from telegram import Update
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes
 
@@ -8,11 +11,14 @@ from logger import CryptoLogger
 file_config = 'config.json'
 config = Config(file_config)
 config.read_config_file()
-logger = CryptoLogger("telegram_message", config.log_file)
+logger = CryptoLogger(config,"telegram_message")
 
 async def get_text_form_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.effective_message
-    logger.info(f"Get message '{message.text}'")
+    logger.info(f"Get message '{message.text}' (id={message.id}, date={message.date})")
+    if (datetime.datetime.now(pytz.utc) -message.date).seconds > config.telegram_skip_message_seconds:
+        logger.info(f"Skip message id={message.id}")
+        return
     parse_telegram_message.parse_message(message.text)
 
 
